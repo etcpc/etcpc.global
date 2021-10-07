5de0e8ca62dee32669a8670d55bd17f35782bb34
Wendirad Demelash <wendiradame@gmail.com>
2021-10-07 23:04:51 +0300

    [style] Update formating

--- src/etcpc.global/etcpc/urls.py
+++ src/etcpc.global/etcpc/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import include, path
 
 urlpatterns = [
-    
-        path('jet/', include('jet.urls', 'jet')),  # Django JET URLS  
-        path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
-        path('admin/', admin.site.urls),
+    path("jet/", include("jet.urls", "jet")),  # Django JET URLS
+    path("jet/dashboard/", include("jet.dashboard.urls", "jet-dashboard")),
+    path("admin/", admin.site.urls),
 ]
