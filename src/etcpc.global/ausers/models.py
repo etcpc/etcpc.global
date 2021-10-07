ceab43126c8f474fd31e8c03b58a7d8cad14d3a3
Wendirad Demelash <wendiradame@gmail.com>
2021-10-07 16:33:39 +0300

    [feat] Create model Address
    + An abstract class that define composite attributes for address

--- src/etcpc.global/ausers/models.py
+++ src/etcpc.global/ausers/models.py
@@ -8,3 +8,29 @@
 	Last modified by: Wendirad Demelash
 """
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from ausers.validators import PhoneNumberValidator
+
+
+class Address(models.Model):
+    """An abstract class that represent composite attributes of
+    it's subclasses.
+
+    Attributes:
+            email: an email field, used to store email address of subclass instances
+            phone_number: character field, that store formated phone numbers
+    """
+
+    email = models.EmailField(_("email address"), unique=True)
+    phone_number = models.CharField(
+        _("phone number"),
+        max_length=13,
+        validators=(PhoneNumberValidator,),
+        help_text=_(
+            "Formated phone number used as an alternative address for subclasses."
+        ),
+    )
+
+    class Meta:
+        abstract = True
