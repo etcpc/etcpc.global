8ffac19e953eaca3bf76ef4173fa85d003219078
Esraeldawit-a <esraeldawit0@gmail.com>
2021-10-07 22:31:06 +0300

    [feat] django jet3 dashboard added

--- src/etcpc/etcpc/settings.py
+++ src/etcpc/etcpc/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jet.dashboard',
+    'jet',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -70,12 +72,48 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "etcpc.wsgi.application"
 
+JET_THEMES = [
+    {
+        'theme': 'default', # theme folder name
+        'color': '#47bac1', # color of the theme's button in user menu
+        'title': 'Default' # theme title
+    },
+    {
+        'theme': 'green',
+        'color': '#44b78b',
+        'title': 'Green'
+    },
+    {
+        'theme': 'light-green',
+        'color': '#2faa60',
+        'title': 'Light Green'
+    },
+    {
+        'theme': 'light-violet',
+        'color': '#FFFF00',
+        'title': 'Light Violet'
+    },
+    {
+        'theme': 'light-blue',
+        'color': '#5EADDE',
+        'title': 'Light Blue'
+    },
+    {
+        'theme': 'light-gray',
+        'color': '#222',
+        'title': 'Light Gray'
+    }
+]
+
+
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+WSGI_APPLICATION = "etcpc.wsgi.application"
+JET_DEFAULT_THEME = 'light-violet'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     "default": config(
         "DATABASE_URL", default="sqlite:///" / BASE_DIR / "db.sqlite3", cast=db_url
