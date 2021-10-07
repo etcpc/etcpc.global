d80b2b2649816a6172044e97645c0b76a9be79f8
Wendirad Demelash <wendiradame@gmail.com>
2021-10-07 15:29:44 +0300

    [style] Change formating

--- src/etcpc.global/etcpc/wsgi.py
+++ src/etcpc.global/etcpc/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'etcpc.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "etcpc.settings")
 
 application = get_wsgi_application()
