f8146e6dcd1bdad9dbde1471545d025d2413d582
Ruth Retta <ruthretta2000@gmail.com>
2021-10-08 18:47:41 +0300

    [feat] Add 'blog' app url

--- src/etcpc.global/etcpc/urls.py
+++ src/etcpc.global/etcpc/urls.py
@@ -21,6 +21,7 @@ from django.urls import include, path
 urlpatterns = [
     path("jet/", include("jet.urls", "jet")),  # Django JET URLS
     path("jet/dashboard/", include("jet.dashboard.urls", "jet-dashboard")),
+    path("", include("blog.urls")),
     path("admin/", admin.site.urls),
 ]
 
