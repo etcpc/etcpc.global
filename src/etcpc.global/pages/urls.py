5ee76cb2f97ce548911d15481b80fdc97ba64c75
Ruth Retta <ruthretta2000@gmail.com>
2021-10-08 18:49:41 +0300

    [feat] Add url pattern
    * Add HomePage view

--- src/etcpc.global/pages/urls.py
+++ src/etcpc.global/pages/urls.py
@@ -4,5 +4,9 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-	path('home/',TemplateView.as_view(template_name='pages/index.html'), name="home-page"),
+    path(
+        "home/",
+        TemplateView.as_view(template_name="pages/index.html"),
+        name="home-page",
+    ),
 ]
